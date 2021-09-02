9697be220b4592dbba0307d6102f2c7f86fcddfd
Calvin Donner <calvinjdonner@gmail.com>
2021-09-02 12:47:03 -0600

    button clicks to update display city featured in main box

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 var currentDate = moment().format('M/DD/YYYY');
-var displayCity = $("#display-city");
+var displayCity = document.getElementById("display-city"); //jQuery $ function not working, why???
+var selectedCity = document.getElementById("austin");
 
-$("#austin").click(function(){
-    displayCity.textContent = "Austin (" +currentDate + ")";
+$("#austin").on("click", function() {
+    displayCity.textContent = selectedCity.textContent + " (" + currentDate + ")";
     console.log("austin was clicked");
-})
+}) //how to make this function apply to all buttons? 
+
