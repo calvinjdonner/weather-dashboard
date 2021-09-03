bbf297d59a845f1388b136db6e70fff7b41c8eaa
Calvin Donner <calvinjdonner@gmail.com>
2021-09-02 21:09:30 -0600

    added api variable

--- script.js
+++ script.js
@@ -1,9 +1,32 @@
 var currentDate = moment().format('M/DD/YYYY');
-var displayCity = document.getElementById("display-city"); //jQuery $ function not working, why???
-var selectedCity = document.getElementById("austin");
+var displayCity = $("#display-city");
+var selectedCity = $("#austin")[0].outerText;
+var weather = {
+    "apiKey": "1a0ab489aaca085005fda81811f9c8cc"
+}
 
 $("#austin").on("click", function() {
-    displayCity.textContent = selectedCity.textContent + " (" + currentDate + ")";
-    console.log("austin was clicked");
+    displayCity.text(selectedCity + " (" + currentDate + ")");
+    console.log(displayCity);
+    console.log($("#austin")[0].outerText);
 }) //how to make this function apply to all buttons? 
 
+
+var searchCity = function() {
+    var searchTerm = document.getElementById("display-city").value;
+fetch("https://api.openweathermap.org/data/2.5/onecall?lat= {lat} &lon= {lon} &exclude={part} &appid=1a0ab489aaca085005fda81811f9c8cc")
+
+console.log($("#display-city").value)
+.then(function(response) {
+    return response.json();
+})
+}
+
+
+//for loop to display emojis based on weather conditions. Need emoji in #display-city, and in each card under the anchor element
+
+//how to handle search function? Create cities array with listed cities as well as any search terms? 
+
+//open weather API uses lat/lon. Google maps platform using reverse geocoding (address lookup) ??? 
+
+//fetch("https://maps.googleapis.com/maps/api/geocode/outputFormat?parameters") use locality for address type
