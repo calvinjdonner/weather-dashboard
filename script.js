029732061e9b6b0cc40b4f3b976c5e39266a917f
Calvi <calvinjdonner@gmail.com>
2021-09-04 08:22:28 -0700

    added cities button functionality

--- script.js
+++ script.js
@@ -1,43 +1,39 @@
 var currentDate = moment().format('M/DD/YYYY');
 var displayCity = $("#display-city");
-var selectedCity = $("#austin")[0].outerText;
+
+$(".cities").click(function() {
+    var selectedCity = this.outerText;
+    displayCity.text(selectedCity + " (" + currentDate + ")");
+});
 
 var cityWeather = {
     apiKey: "1a0ab489aaca085005fda81811f9c8cc",
-    fetchWeather: function (displayCity) {
-        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + displayCity + "&appid=" + this.apiKey)
+    fetchWeather: function () {
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + selectedCity + "&appid=" + this.apiKey)
             .then((response) => response.json())
             .then((data) => console.log(data));
     },
     displayCoordinates: function(data) {
         const { lon, lat } = data.coord;
-        console.log(lon, lat);
-    }
+        var coordinates = lon && lat;
+        console.log(coordinates);
+        }
 };
 
 var weather = {
-    "apiKey": "1a0ab489aaca085005fda81811f9c8cc",
-    fetchWeather: function () {
-        fetch("https://api.openweathermap.org/data/2.5/onecall?lat=30.2672&lon=-97.7431&units=imperial&appid=1a0ab489aaca085005fda81811f9c8cc")
+    apiKey: "1a0ab489aaca085005fda81811f9c8cc",
+    fetchWeather: function (city) {
+        fetch("https://api.openweathermap.org/data/2.5/onecall?lat=30.2672&lon=-97.7431&units=imperial&appid=" + this.apiKey)
             .then((response) => response.json())
-            .then((data) => console.log(data));
+            .then((data) => this.displayWeather(data));
     },
 
     displayWeather: function(data) {
         const { temp, wind_speed, humidity, uvi, } = data.current;
-        const { icon } = data.weather;
+        const { icon } = data.weather[0];
         console.log(temp, wind_speed, humidity, uci, icon);
     }
 };
-
-
-$("#austin").on("click", function() {
-    displayCity.text(selectedCity + " (" + currentDate + ")");
-    console.log(displayCity);
-    console.log($("#austin")[0].outerText);
-    console.log(weather);
-}) //how to make this function apply to all buttons? 
-
 
 var searchCity = function() {
     var searchTerm = document.getElementById("display-city").value;
