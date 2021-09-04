9019b426ff8956b4de5d300c35c3b83ddcc34b8c
Calvi <calvinjdonner@gmail.com>
2021-09-03 21:32:57 -0600

    added fetch weather APIs

--- script.js
+++ script.js
@@ -1,20 +1,46 @@
 var currentDate = moment().format('M/DD/YYYY');
 var displayCity = $("#display-city");
 var selectedCity = $("#austin")[0].outerText;
+
+var cityWeather = {
+    apiKey: "1a0ab489aaca085005fda81811f9c8cc",
+    fetchWeather: function (displayCity) {
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + displayCity + "&appid=" + this.apiKey)
+            .then((response) => response.json())
+            .then((data) => console.log(data));
+    },
+    displayCoordinates: function(data) {
+        const { lon, lat } = data.coord;
+        console.log(lon, lat);
+    }
+};
+
 var weather = {
-    "apiKey": "1a0ab489aaca085005fda81811f9c8cc"
-}
+    "apiKey": "1a0ab489aaca085005fda81811f9c8cc",
+    fetchWeather: function () {
+        fetch("https://api.openweathermap.org/data/2.5/onecall?lat=30.2672&lon=-97.7431&units=imperial&appid=1a0ab489aaca085005fda81811f9c8cc")
+            .then((response) => response.json())
+            .then((data) => console.log(data));
+    },
+
+    displayWeather: function(data) {
+        const { temp, wind_speed, humidity, uvi, } = data.current;
+        const { icon } = data.weather;
+        console.log(temp, wind_speed, humidity, uci, icon);
+    }
+};
+
 
 $("#austin").on("click", function() {
     displayCity.text(selectedCity + " (" + currentDate + ")");
     console.log(displayCity);
     console.log($("#austin")[0].outerText);
+    console.log(weather);
 }) //how to make this function apply to all buttons? 
 
 
 var searchCity = function() {
     var searchTerm = document.getElementById("display-city").value;
-fetch("https://api.openweathermap.org/data/2.5/onecall?lat= {lat} &lon= {lon} &exclude={part} &appid=1a0ab489aaca085005fda81811f9c8cc")
 
 console.log($("#display-city").value)
 .then(function(response) {
@@ -22,6 +48,7 @@ console.log($("#display-city").value)
 })
 }
 
+//heebyjeeby
 
 //for loop to display emojis based on weather conditions. Need emoji in #display-city, and in each card under the anchor element
 
