7b7582cf307e08a623904c4a6f0405cc9b9f6211
Calvin Donner <calvinjdonner@gmail.com>
2021-11-01 10:12:30 -0600

    refactor entire project

--- script.js
+++ script.js
@@ -1,65 +1,169 @@
-var currentDate = moment().format('M/DD/YYYY');
-var displayCity = $("#display-city");
-city = [];
-var citySearch = $("#citySearch");
+var owmAPI = "1a0ab489aaca085005fda81811f9c8cc";
+var currentCity = "";
+var lastCity = "";
 
-$(".cities").click(function() {
-    var selectedCity = this.outerText;
-    citySearch.text(selectedCity);
-    console.log(this);
-    city.push(this.outerText);
-    displayCity.text(selectedCity + " (" + currentDate + ")");
-});
-
-var cityWeather = {
-    apiKey: "1a0ab489aaca085005fda81811f9c8cc",
-    fetchWeather: function (city) {
-        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + this.apiKey)
-            .then((response) => response.json())
-            .then((data) => console.log(data));
-    },
-    displayCoordinates: function(data) {
-        const { lon, lat } = data.coord;
-        var coordinates = lon && lat;
-        console.log(coordinates);
-        }
-};
-
-var weather = {
-    apiKey: "1a0ab489aaca085005fda81811f9c8cc",
-    fetchWeather: function (city) {
-        fetch("https://api.openweathermap.org/data/2.5/onecall?lat=30.2672&lon=-97.7431&units=imperial&appid=" + this.apiKey)
-            .then((response) => response.json())
-            .then((data) => this.displayWeather(data));
-    },
-
-    displayWeather: function(data) {
-        const { temp, wind_speed, humidity, uvi, } = data.current;
-        const { icon } = data.weather[0];
-        console.log(temp, wind_speed, humidity, uvi, icon);
-        $("#icon").src = "https://openweathermap.org/img/wn/" +icon + ".png";
-        $("#temp").innertext = temp;
-        $("#wind").innertext = wind_speed;
-        $("#humidity").innertext = humidity;
-        $("#uvi").innertext = uvi
+var handleErrors = (response) => {
+    if (!response.ok) {
+        throw Error(response.statusText);
     }
-};
-
-var searchCity = function() {
-    var searchTerm = document.getElementById("display-city").value;
-
-console.log($("#display-city").value)
-.then(function(response) {
-    return response.json();
-})
+    return response;
 }
 
-//heebyjeeby
+var getCurrentConditions = (event) => {
+    let city = $('#search-city').val();
+    currentCity= $('#search-city').val();
+    let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial" + "&APPID=" + owmAPI;
+    fetch(queryURL)
+    .then(handleErrors)
+    .then((response) => {
+        return response.json();
+    })
+    .then((response) => {
+        saveCity(city);
+        $('#search-error').text("");
+        let currentWeatherIcon="https://openweathermap.org/img/w/" + response.weather[0].icon + ".png";
+        let currentTimeUTC = response.dt;
+        let currentTimeZoneOffset = response.timezone;
+        let currentTimeZoneOffsetHours = currentTimeZoneOffset / 60 / 60;
+        let currentMoment = moment.unix(currentTimeUTC).utc().utcOffset(currentTimeZoneOffsetHours);
+        renderCities();
+        getFiveDayForecast(event);
+        let currentWeatherHTML = `
+            <h3>${response.name} ${currentMoment.format("(MM/DD/YY)")}<img src="${currentWeatherIcon}"></h3>
+            <ul class="list-unstyled">
+                <li>Temperature: ${response.main.temp}&#8457;</li>
+                <li>Humidity: ${response.main.humidity}%</li>
+                <li>Wind Speed: ${response.wind.speed} mph</li>
+                <li id="uvIndex">UV Index:</li>
+            </ul>`;
+        $('#current-weather').html(currentWeatherHTML);
+        let latitude = response.coord.lat;
+        let longitude = response.coord.lon;
+        let uvQueryURL = "api.openweathermap.org/data/2.5/uvi?lat=" + latitude + "&lon=" + longitude + "&APPID=" + owmAPI;
+        uvQueryURL = "https://cors-anywhere.herokuapp.com/" + uvQueryURL;
+        fetch(uvQueryURL)
+        .then(handleErrors)
+        .then((response) => {
+            return response.json();
+        })
+        .then((response) => {
+            let uvIndex = response.value;
+            $('#uvIndex').html(`UV Index: <span id="uvVal"> ${uvIndex}</span>`);
+            if (uvIndex>=0 && uvIndex<3){
+                $('#uvVal').attr("class", "uv-favorable");
+            } else if (uvIndex>=3 && uvIndex<8){
+                $('#uvVal').attr("class", "uv-moderate");
+            } else if (uvIndex>=8){
+                $('#uvVal').attr("class", "uv-severe");
+            }
+        });
+    })
+}
 
-//for loop to display emojis based on weather conditions. Need emoji in #display-city, and in each card under the anchor element
+var getFiveDayForecast = (event) => {
+    let city = $('#search-city').val();
+    let queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial" + "&APPID=" + owmAPI;
+    fetch(queryURL)
+        .then (handleErrors)
+        .then((response) => {
+            return response.json();
+        })
+        .then((response) => {
+        // HTML template
+        let fiveDayForecastHTML = `
+        <h2>5-Day Forecast:</h2>
+        <div id="fiveDayForecastUl" class="d-inline-flex flex-wrap ">`;
+        for (let i = 0; i < response.list.length; i++) {
+            let dayData = response.list[i];
+            let dayTimeUTC = dayData.dt;
+            let timeZoneOffset = response.city.timezone;
+            let timeZoneOffsetHours = timeZoneOffset / 60 / 60;
+            let thisMoment = moment.unix(dayTimeUTC).utc().utcOffset(timeZoneOffsetHours);
+            let iconURL = "https://openweathermap.org/img/w/" + dayData.weather[0].icon + ".png";
+            // Only displaying mid-day forecasts
+            if (thisMoment.format("HH:mm:ss") === "11:00:00" || thisMoment.format("HH:mm:ss") === "12:00:00" || thisMoment.format("HH:mm:ss") === "13:00:00") {
+                fiveDayForecastHTML += `
+                <div class="weather-card card m-2 p0">
+                    <ul class="list-unstyled p-3">
+                        <li>${thisMoment.format("MM/DD/YY")}</li>
+                        <li class="weather-icon"><img src="${iconURL}"></li>
+                        <li>Temp: ${dayData.main.temp}&#8457;</li>
+                        <li>Wind: ${dayData.wind.speed}MPH</li>
+                        <li>Humidity: ${dayData.main.humidity}%</li>
+                    </ul>
+                </div>`;
+            }
+        }
+        fiveDayForecastHTML += `</div>`;
+        $('#five-day-forecast').html(fiveDayForecastHTML);
+    })
+}
 
-//how to handle search function? Create cities array with listed cities as well as any search terms? 
+var saveCity = (newCity) => {
+    let cityExists = false;
+    for (let i = 0; i < localStorage.length; i++) {
+        if (localStorage["cities" + i] === newCity) {
+            cityExists = true;
+            break;
+        }
+    }
+    if (cityExists === false) {
+        localStorage.setItem('cities' + localStorage.length, newCity);
+    }
+}
 
-//open weather API uses lat/lon. Google maps platform using reverse geocoding (address lookup) ??? 
+var renderCities = () => {
+    $('#city-results').empty();
+    if (localStorage.length===0){
+        if (lastCity){
+            $('#search-city').attr("value", lastCity);
+        } else {
+            $('#search-city').attr("value", "Austin");
+        }
+    } else {
+        let lastCityKey="cities"+(localStorage.length-1);
+        lastCity=localStorage.getItem(lastCityKey);
+        $('#search-city').attr("value", lastCity);
+        for (let i = 0; i < localStorage.length; i++) {
+            let city = localStorage.getItem("cities" + i);
+            let cityEl;
+            if (currentCity===""){
+                currentCity=lastCity;
+            }
+            if (city === currentCity) {
+                cityEl = `<button type="button" class="list-group-item list-group-item-action active">${city}</button></li>`;
+            } else {
+                cityEl = `<button type="button" class="list-group-item list-group-item-action">${city}</button></li>`;
+            } 
+            $('#city-results').prepend(cityEl);
+        }
+        if (localStorage.length>0){
+            $('#clear-storage').html($('<a id="clear-storage" href="#">clear</a>'));
+        } else {
+            $('#clear-storage').html('');
+        }
+    }
+    
+}
 
-//fetch("https://maps.googleapis.com/maps/api/geocode/outputFormat?parameters") use locality for address type
+$('#search-button').on("click", (event) => {
+event.preventDefault();
+currentCity = $('#search-city').val();
+getCurrentConditions(event);
+});
+
+$('#city-results').on("click", (event) => {
+    event.preventDefault();
+    $('#search-city').val(event.target.textContent);
+    currentCity=$('#search-city').val();
+    getCurrentConditions(event);
+});
+
+$("#clear-storage").on("click", (event) => {
+    localStorage.clear();
+    renderCities();
+});
+
+renderCities();
+
+getCurrentConditions();
